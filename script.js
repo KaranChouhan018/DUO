5631d2506bd94d7c0be157f8f8c33197d56b3fec
meinhoonkaran <120255877+meinhoonkaran@users.noreply.github.com>
2023-06-08 18:10:17 +0530

    h

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ gsap.to( 'video', {
         pin: ".div2"
     },
     width: "95%" ,
-    Backgroundposition: '200px 0px' ,
+    backgroundPosition: '200px 0px' ,
     ease: "Expo.easeInOut"
 })
-gsap.registerPlugin(CSS),
 gsap.from( '.div3 ,.b7' ,{
     scrollTrigger: {
         trigger: ".about" ,
@@ -20,19 +19,49 @@ gsap.from( '.div3 ,.b7' ,{
     },
     opacity: 0 ,
     y : -50,
-    stagger:0.8
+    stagger:0.8,
 })
-gsap.from('.work h1 , .img-wrapper' , {
+gsap.from('.work h1', {
     scrollTrigger: {
         trigger: ".work" ,
         start: "top top" ,
-        scrub: 1.5 ,
+        scrub: 2 ,
         pin: ".work"
     },
     opacity:0,
-    y:-50,
-    stagger:1
+    x:-50,
 })
+gsap.fromTo('.img-wrapper:nth-child(1), .img-wrapper:nth-child(3)',{
+    opacity:0,
+    x:300,
+},{
+    opacity:1,
+    x:0,
+    duration:1,
+    delay:0.5,
+    scrollTrigger:".card-container"
+})
+gsap.fromTo('.img-wrapper:nth-child(2), .img-wrapper:nth-child(4)',{
+    opacity:0,
+    x:-300,
+},{
+    opacity:1,
+    x:0,
+    duration:1,
+    delay:1,
+    scrollTrigger:".img-wrapper:nth-child(1)"
+})
+gsap.fromTo('.btn__circle',{
+    opacity:0,
+    x:300,
+},{
+    opacity:1,
+    x:0,
+    duration:1,
+    delay:1.5,
+    scrollTrigger:".row"
+})
+
 
 gsap.from('.navbar ' , {opacity : 0 , duration: 2 , y : -50})
 gsap.from('.b1', { opacity: 0 , duration: 2 , x: 100 , delay: 1.2} )
