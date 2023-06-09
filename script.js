17c60e1f68ce1abc09b9624f4734dc6fb4d193da
meinhoonkaran <120255877+meinhoonkaran@users.noreply.github.com>
2023-06-09 22:58:03 +0530

    G

--- script.js
+++ script.js
@@ -1,68 +1,130 @@
-gsap.to( 'video', {
-    scrollTrigger: {
-        trigger: ".div2" ,
-        start: "top top" ,
-        scrub: 1.5 ,
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  // follwoing line is not required to work pinning on touch screen
+
+  /* pinType: document.querySelector(".main").style.transform
+    ? "transform"
+    : "fixed"*/
+});
+
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+gsap.to( 'video' ,{
+    scrollTrigger : {
+        scroller : 'body',
+        trigger:".div2",
+        start:"top top",
+        end: " top -10",
+        scrub : 2,
+
         pin: ".div2"
     },
-    width: "95%" ,
-    backgroundPosition: '200px 0px' ,
-    ease: "Expo.easeInOut"
+    width: "100%",
+    ease: "Expo.easeInout"
 })
-gsap.from( '.div3 ,.b7' ,{
-    scrollTrigger: {
-        trigger: ".about" ,
-        scroller:"body" ,
-        start: "top top" ,
-        scrub: 2 ,
-        pin: ".about"
-    },
-    opacity: 0 ,
-    y : -50,
-    stagger:0.8,
+gsap.from( '.img-wrapper:nth-child(1),.img-wrapper:nth-child(3)' ,{
+  scrollTrigger : {
+      scroller : 'body',
+      trigger:".card-container",
+      start:"top top",
+      end: " top -10",
+      scrub : 2,
+  },
+  opacity:0,
+  x:200,
+  stagger:1,
+  ease: "Expo.easeInout"
 })
-gsap.from('.work h1', {
-    scrollTrigger: {
-        trigger: ".work" ,
-        start: "top top" ,
-        scrub: 2 ,
-        pin: ".work"
-    },
+gsap.from( '.img-wrapper:nth-child(2),.img-wrapper:nth-child(4)' ,{
+  scrollTrigger : {
+      scroller : 'body',
+      trigger:".card-container",
+      start:"top top",
+      end: " top -10",
+      scrub : 2,
+  },
+  opacity:0,
+  x:-200,
+  stagger:1,
+  ease: "Expo.easeInout"
+})
+gsap.fromTo('.work .b8 ',{
+  opacity:0,
+  x:-200,
+},{
+  opacity:1,
+  x:0,
+  duration:1,
+  start:"top top",
+  scrollTrigger:".work"
+})
+gsap.fromTo('.btn__circle ',{
     opacity:0,
-    x:-50,
-})
-gsap.fromTo('.img-wrapper:nth-child(1), .img-wrapper:nth-child(3)',{
-    opacity:0,
-    x:300,
+    x:200,
 },{
     opacity:1,
     x:0,
     duration:1,
-    delay:0.5,
-    scrollTrigger:".card-container"
-})
-gsap.fromTo('.img-wrapper:nth-child(2), .img-wrapper:nth-child(4)',{
-    opacity:0,
-    x:-300,
-},{
-    opacity:1,
-    x:0,
-    duration:1,
-    delay:1,
-    scrollTrigger:".img-wrapper:nth-child(1)"
-})
-gsap.fromTo('.btn__circle',{
-    opacity:0,
-    x:300,
-},{
-    opacity:1,
-    x:0,
-    duration:1,
-    delay:1.5,
+    start:"top top",
+    end:"top -10",
     scrollTrigger:".row"
 })
 
+gsap.fromTo('.b6 .b9',{
+  opacity:0,
+  x:-200,
+},{
+  opacity:1,
+  x:0,
+  duration:1,
+  start:"top top",
+  end:"top -10",
+  scrollTrigger:".row"
+})
 
+
+gsap.to("#ca", {
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".hover",
+      start: "top 65%",
+      end: "#below",
+      scrub: 1
+    },
+    left: "-40%"
+  })
+  
 gsap.from('.navbar ' , {opacity : 0 , duration: 2 , y : -50})
 gsap.from('.b1', { opacity: 0 , duration: 2 , x: 100 , delay: 1.2} )
 gsap.from('.b2', { opacity: 0 , duration: 2 , x: -50 , delay:1.4 } )
